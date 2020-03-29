Clean code overall. One fix left : Normalization Next step : Implement TensorBoard

bot.py
@@ -11,15 +11,18 @@ from game import ConnectFourGame
 
 
 EPISODES = 1_000
-UPDATE_TARGET_MODEL_EVERY = 50
+UPDATE_TARGET_MODEL_EVERY = 200
 REPLAY_MEMORY_SIZE = 10_000
+MIN_TRAIN_SAMPLE = 100
 BATCH_SIZE = 32
 GAMMA = 0.98
+EPSILON = 0.9
+EPSILON_DECAY = 0.99
+MIN_EPSILON = 0.1
+
+RENDER_EVERY = 50
+SHOW_GAME_OVER = True
 MAX_ACTIONS = 7 * 6
-
-ACTION_SPACE = 7
-
-UNAUTHORIZED = -1
 
 class OneHotEncoder:
 
@@ -45,7 +48,10 @@ class AgentRadnom(Player):
 
 class AgentDQN(Player):
 
-	def __init__(self, model=None, replay_memory=None):
+	def __init__(self, param, model=None, replay_memory=None):
+		# Game settings
+		self.param = param
+
 		# Unstable Model
 		if model != None:
 			self.model = model
@@ -64,17 +70,44 @@ class AgentDQN(Player):
 			self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
 
 		# One Hot Encoder / Decoder
-		self.codec = OneHotEncoder(ACTION_SPACE-1)
+		self.codec = OneHotEncoder(self.param["ACTION_SPACE"]-1)
 
-	def play(self, state):
+	def create_model(self, model=None):
+		if model == None:
+			model = Sequential()
+
+			model.add(Convolution2D(8, (4,4), activation="relu", padding="same", input_shape=(7, 6, 1)))
+			model.add(Flatten())
+			model.add(Dense(32, activation="relu"))
+			model.add(Dense(16, activation="relu"))
+			model.add(Dense(self.param["ACTION_SPACE"], activation="relu"))
+
+			model.compile(optimizer=Adam(), loss="mse")
+
+		return model
+
+	def play(self, state, use_target_model=False):
+		global EPSILON
 		# First compute the Q-Values, then return the index with the highest Q-Value aka the action
-		return np.argmax(self.model.predict(np.reshape(state, (1,7,6,1))))
+		if use_target_model:
+			return np.argmax(self.target_model.predict(np.reshape(state, (1,7,6,1)))[0])
+		else:
+			# Decay epsilon
+			EPSILON = max(MIN_EPSILON, EPSILON * EPSILON_DECAY)
 
-	def update_replay_memory(self, state, action, reward, new_state):
-		self.replay_memory.append((state, action, reward, new_state))
+			if EPSILON < random.random():
+				return random.randint(0,6)
+			else:
+				return np.argmax(self.model.predict(np.reshape(state, (1,7,6,1)))[0])
 
-	def train(self, value, compute_new_state=None):
-		if len(self.replay_memory) < BATCH_SIZE:
+	def update_replay_memory(self, state, player, action, reward, new_state):
+		self.replay_memory.append((state, player, action, reward, new_state))
+
+	def update_target_model(self):
+		self.target_model.set_weights(self.model.get_weights())
+
+	def train(self, compute_new_state=None):
+		if len(self.replay_memory) < MIN_TRAIN_SAMPLE:
 			return
 
 		sample = random.sample(self.replay_memory, BATCH_SIZE)
@@ -83,49 +116,32 @@ class AgentDQN(Player):
 		y = [] 
 
 		for i in range(BATCH_SIZE):
-			state, action, reward, opponent_state = sample[i]
+			state, player, action, reward, opponent_state = sample[i]
 
-			opponent_action = np.argmax(self.target_model.predict(np.reshape(opponent_state, (1, 7, 6, 1)))[0]) # Opponent makes the best possible action
+			opponent_player = -1 * player
+			opponent_action = self.play(opponent_state, use_target_model=True) # Opponent makes the best possible action
 
 			if compute_new_state != None:
-				new_state = compute_new_state(opponent_state, opponent_action, value) # New State after opponent has played
+				new_state = compute_new_state(opponent_state, opponent_player, opponent_action) # New State after opponent has played
 			else:
 				new_state = opponent_state.copy()
 				if new_state[opponent_action][0] == 0: # Free spot in that column
 					for row in range(5, -1, -1):
 						if new_state[opponent_action][row] == 0:
-							new_state[opponent_action][row] = value # Assign given value
+							new_state[opponent_action][row] = opponent_player # Assign given value
 
 			target = reward + GAMMA * max(self.target_model.predict(np.reshape(new_state, (1, 7, 6, 1)))) # The target Q-Value of the played action
 
 			q_values = self.model.predict(np.reshape(new_state, (1, 7, 6, 1)))[0]
 
-			for j in range(ACTION_SPACE):
+			for j in range(self.param["ACTION_SPACE"]):
 				if j == action:
 					q_values[j] == target
 
 				if new_state[j][0] != 0: # Check if action j is not possible
-					q_values[j] == UNAUTHORIZED
+					q_values[j] == self.param["UNAUTHORIZED"]
 
 			x.append(state)
 			y.append(q_values)
 
 		self.model.fit(np.array(x), np.array(y), verbose=0)
-
-	def create_model(self, model=None):
-		if model == None:
-			model = Sequential()
-
-			model.add(Convolution2D(8, (4,4), padding="same", input_shape=(7, 6, 1))) # , input_shape=(7,6)
-			model.add(Flatten())
-			model.add(Dense(32))
-			model.add(Dense(16))
-			model.add(Dense(ACTION_SPACE))
-
-			model.compile(optimizer=Adam(), loss="mse")
-
-		return model
-
-
-	def update_target_model(self):
-		self.target_model.set_weights(self.model.get_weights())